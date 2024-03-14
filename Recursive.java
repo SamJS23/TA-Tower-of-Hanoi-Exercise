15c84878e3a4a5615c49cc3ff1c0157d1f70fc2e
SamJS23 <144894556+SamJS23@users.noreply.github.com>
2024-03-14 19:07:47 +0700

    Add files via upload

---
+++ Recursive.java
@@ -0,0 +1,21 @@
+import java.io.*;
+import java.math.*;
+import java.util.*;
+public class Recursive {
+    public static void main(String[] args){
+        int n = 3;
+        shiftDisk(n, 'A','B','C');
+    }
+    public static void shiftDisk(int n, char TA, char TB, char TC){
+        if(n == 1){
+            System.out.println("Disk 1 is shifted from " + TA + " to " + TC);
+        } else{
+            shiftDisk(n-1, TA, TC, TB);
+            System.out.println("Disk " + n +" from " + TA + " is shifted to " + TC);
+            shiftDisk(n-1, TB, TA, TC);
+
+        }
+    }
+}
+
+
